58f57d07caa35356e8e010031efb8b26f5b613d2
Sang-Ki Ko <narame7@gmail.com>
2021-08-03 00:13:10 +0900

    revise main.py and split.py to make prototype model works

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', default='./data/train.csv', dest='data_path',
                     help='Path to data')
 parser.add_argument('--batch_size', action='store', dest='batch_size',
-                    help='batch size', default=4)
+                    help='batch size', default=1)
 parser.add_argument('--checkpoint_pos', default='./saved_models/hidden_512/best_model', dest='checkpoint_pos',
                     help='path to checkpoint for splitting positive strings ')
 parser.add_argument('--checkpoint_neg', default='./saved_models/hidden_512/best_model', dest='checkpoint_neg',
@@ -18,19 +18,38 @@ parser.add_argument('--sub_model', action='store', dest='sub_model', default='se
 
 opt = parser.parse_args()
 
-data = dataset.get_loader(opt.data_path, batch_size=opt.batch_size, shuffle=False)
-pos_checkpoint = Checkpoint.load(Checkpoint.get_latest_checkpoint(opt.checkpoint_pos))
-neg_checkpoint = Checkpoint.load(Checkpoint.get_latest_checkpoint(opt.checkpoint_neg))
 
-pos_split_model = pos_checkpoint.model
-neg_split_model = neg_checkpoint.model
 
-pos_split_model.eval()
-neg_split_model.eval()
 
-with torch.no_grad():
+
+def print_tensor_set(tensor_set):
+    output_strings = ''.join(map(str, tensor_set[0][tensor_set[0] != 10].tolist()))
+    for i in range(1, tensor_set.shape[0]):
+        output_strings += ', ' + ''.join(map(str, tensor_set[i][tensor_set[i] != 10].tolist()))
+    
+    print(output_strings)
+
+    
+def main():    
+    data = dataset.get_loader(opt.data_path, batch_size=opt.batch_size, shuffle=False)
+    
+    pos_checkpoint = Checkpoint.load(Checkpoint.get_latest_checkpoint(opt.checkpoint_pos))
+    neg_checkpoint = Checkpoint.load(Checkpoint.get_latest_checkpoint(opt.checkpoint_neg))
+
+    pos_split_model = pos_checkpoint.model
+    neg_split_model = neg_checkpoint.model
+
+    pos_split_model.eval()
+    neg_split_model.eval()
+
     for pos, neg, regex in data:
         pos, neg, regex = dataset.batch_preprocess(pos, neg, regex)
+
+        print('Positive Strings')
+        print_tensor_set(pos[0])
+        print('Negative Strings')
+        print_tensor_set(neg[0])
+        print('Target Regex:', ''.join(regex[0]))
 
         _, _, other = pos_split_model(pos, None, regex)
         splited_pos = split(pos, other['sequence'])  # batch, set, seq
@@ -42,5 +61,8 @@ with torch.no_grad():
         for batch_idx in range(len(pos)):
             batch_predict.append(generate_split_regex(splited_pos[batch_idx], splited_neg[batch_idx]))
 
-        print(batch_predict)
+        print('Generated Regex:', batch_predict[0])
 
+        
+if __name__ == "__main__":
+    main()
--- seq2seq/util/split.py
+++ seq2seq/util/split.py
@@ -1,41 +1,45 @@
 import torch
+
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'submodels', 'SoftConsiceNormalFrom' )))
+
 from collections import Counter
 from submodels.SoftConsiceNormalFrom.synthesizer import synthesis
 from submodels.SoftConsiceNormalFrom.examples import Examples
 
 def split(strings, label):
     label = [i.tolist() for i in label]
-    tmp = torch.Tensor(label).transpose(0, 1).squeeze(-1).tolist()
+    tmp = torch.LongTensor(label).transpose(0, 1).squeeze(-1).tolist()
     predict_dict = [dict(Counter(l)) for l in tmp]
 
     batch = []
     for batch_idx in range(len(strings)):
-        set = []
+        set = []                
         for set_idx in range(10):
+            
+            split_size = strings[batch_idx][strings[batch_idx] != 10].max().item() + 1
+            
             src_seq = strings[batch_idx, set_idx].tolist()  # list of 10 alphabet
             #print(src_seq)
-            predict_seq_dict = predict_dict[batch_idx * 10 + set_idx]  # predict label. ex. {0.0: 2, 1.0: 1, 11.0: 7}
-            #print(predict_seq_dict)
+            predict_seq_dict = predict_dict[batch_idx * 10 + set_idx]  # predict label. ex. {0.0: 2, 1.0: 1, 11.0: 7}            
             seq = []
             idx = 0
-            for seq_id in range(10):
+            for seq_id in range(split_size):
                 tmp = ''
-                if float(seq_id) in predict_seq_dict.keys():
+                if seq_id in predict_seq_dict.keys():
                     for _ in range(predict_seq_dict[float(seq_id)]):
                         tmp += str(src_seq[idx])
                         idx += 1
                 seq.append(tmp)
             #print(seq)
             set.append(seq)
-        #print(set)
         batch.append(set)
 
     return batch
 
 def generate_split_regex(splited_pos, splited_neg):
-
-    print(splited_neg)
-    print(splited_pos)
+    
+    
 
     regex = []
     for sub_id in range(len(splited_pos[0])):
@@ -44,13 +48,24 @@ def generate_split_regex(splited_pos, splited_neg):
 
         for set_idx in range(len(splited_pos)):
             pos.append(splited_pos[set_idx][sub_id])
-            neg.append(splited_neg[set_idx][sub_id])
+            neg.append(splited_neg[set_idx][sub_id])    
 
-        print(list(filter(lambda x : x != '', pos)))
-        print(list(filter(lambda x : x != '', neg)))
-        tmp = synthesis(Examples(pos=list(filter(lambda x : x != '', pos)), neg=list(filter(lambda x : x != '', pos))), 5000)
+        sub_pos_set = set(list(filter(lambda x : x != '', pos)))
+        sub_neg_set = set(list(filter(lambda x : x != '', neg)))
+
+        #print(sub_pos_set)
+        #print(sub_neg_set)
+
+        sub_neg_set -= sub_pos_set
+        
+        print('Splited Positive Strings:', sub_pos_set)
+        print('Splited Negative Strings:', sub_neg_set)
+
+        tmp = synthesis(Examples(pos=sub_pos_set, neg=sub_neg_set), 5000)
+        if tmp is None:
+            return None
         regex.append(tmp)
-        print(tmp)
+        #print(tmp)        
 
     return ''.join(regex)
 
