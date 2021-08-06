f4a6610a5e25091eb98da5c571e78d7206ad2afb
Suhyeon <tngus98207@naver.com>
2021-08-06 11:13:24 +0900

    integrate parsetree.py and revise the route

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -1,3 +1,6 @@
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'submodels', 'SoftConciseNormalForm')))
 from parsetree import *
 from xeger import Xeger
 import argparse
