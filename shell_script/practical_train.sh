74a20a9653a18375f79532df41204c758e015b2c
Suhyeon <tngus98207@naver.com>
2021-10-04 20:23:30 +0900

    script parameter

--- shell_script/practical_train.sh
+++ shell_script/practical_train.sh
@@ -2,5 +2,5 @@
 
 seed=152
 
-python practice_seed.py --seed $seed
-python model_trainer/train.py --train_path ./data/practical_data/integrated/$seed/mega_train.csv --valid_path ./data/practical_data/integrated/$seed/mega_valid.csv --expt_dir saved_models/practical/$seed --gru --hidden_size 256 --num_layer 2 --bidirectional --batch_size 512 --dropout_en 0.5 --dropout_de 0.5 --weight_decay 0.000005 --add_seed $seed
+# python practice_seed.py --seed $seed
+python model_trainer/train.py --train_path ./data/practical_data/integrated/$seed/mega_train.csv --valid_path ./data/practical_data/integrated/$seed/mega_valid.csv --expt_dir saved_models/practical/$seed --gru --hidden_size 256 --num_layer 2 --bidirectional --batch_size 512 --dropout_en 0.4 --dropout_de 0.4 --weight_decay 0.000001 --add_seed $seed
--- shell_script/random_train.sh
+++ shell_script/random_train.sh
@@ -3,4 +3,4 @@
 seed=152
 
 python random_seed.py --seed $seed
-python model_trainer/train.py --train_path ./data/random_data/integrated/$seed/mega_train.csv --valid_path ./data/random_data/integrated/$seed/mega_valid.csv --expt_dir saved_models/random/$seed --gru --hidden_size 256 --num_layer 2 --bidirectional --batch_size 512 --dropout_en 0.5 --dropout_de 0.5 --weight_decay 0.000005 --add_seed $seed
+python model_trainer/train.py --train_path ./data/random_data/integrated/$seed/mega_train.csv --valid_path ./data/random_data/integrated/$seed/mega_valid.csv --expt_dir saved_models/random/$seed --gru --hidden_size 256 --num_layer 2 --bidirectional --batch_size 512 --dropout_en 0.4 --dropout_de 0.4 --weight_decay 0.000001 --add_seed $seed
